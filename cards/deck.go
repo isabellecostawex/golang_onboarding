2a3a7ca288925cdde2fedc8ef0e7d18d0f6822c0
Isabelle Costa <isabelle.costa@wexinc.com>
2024-03-26 09:03:39 -0300

    new prject to shuffle and distribute cards

---
+++ cards/deck.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+	"io/ioutil"
+)
+
+type deck []string
+
+func newDeck() deck {
+	cards := deck{}
+	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
+	cardValues := []string{"Ace", "Two", "Three", "Four"}
+
+	for _, suit := range cardSuits {
+		for _, values := range cardValues {
+			cards = append(cards, suit+" of "+values)
+		}
+	}
+	return cards
+}
+
+func (d deck) print() {
+	for i, card := range d {
+		fmt.Println(i, card)
+	}
+}
+
+func deal(d deck, handSize int) (deck, deck) {
+	return d[:handSize], d[handSize:]
+}
+
+func (d deck) toString() string {
+	return strings.Join([]string(d), ",")
+}
+
+func (d deck) saveToFile(filename string) error {
+	return ioutil.WriteFile(filename, []byte (d.toString()), 0666)
+}
+
+func newDeckFromFile (filename string) deck {
+	byteSlice, err := ioutil.ReadFile(filename)
+	if err != nil{
+		
+	}
+
+}

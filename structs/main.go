f5404cbb9edd56ac09e5b90845a6e99b079e4c84
Isabelle Costa <isabelle.costa@wexinc.com>
2024-03-27 13:23:29 -0300

    Structs

---
+++ structs/main.go
@@ -0,0 +1,37 @@
+package main
+
+import "fmt"
+
+type contactInfo struct {
+	email   string
+	zipCode int
+}
+
+type person struct {
+	firstName string
+	lastName  string
+	contact   contactInfo
+}
+
+func main() {
+	//alex := person{firstName: "Alex", lastName: "Anderson"}
+	jim := person{
+	firstName: "Jim", 
+	lastName: "Party",
+	contact: contactInfo{
+		email: "jim@gmail.com",
+		zipCode: 954400,
+	},
+}
+	JimPointer := &jim
+	JimPointer.updateName("Jimmy")
+	jim.print()
+}
+
+func (pointerToPerson *person) updateName(newFirstName string) {
+	(*pointerToPerson).firstName = newFirstName
+}
+
+func (p person) print() {
+	fmt.Printf("%+v", p)
+}

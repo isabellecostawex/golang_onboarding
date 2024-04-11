7e8e442e28e893baf196fe2d70a6eb2c2a599acc
Isabelle Costa <isabelle.costa@wexinc.com>
2024-04-11 10:20:35 -0300

    new POST method

--- APIs/myapp/main.go
+++ APIs/myapp/main.go
@@ -9,19 +9,29 @@ type Car struct {
 
 var cars []Car
 
-func CreateCars (){
+func generateCars (){
 	cars = append(cars, Car{Name: "Ferrari", Price: 3000000})
 	cars = append(cars, Car{Name: "Mercedes", Price: 4000000})
 	cars = append(cars, Car{Name: "Porsche", Price: 5000000})
 }
 
 func main (){
-	CreateCars()
+	generateCars()
 	e := echo.New()
 	e.GET("/cars", getCars)
-	e.Logger.Fatal(e.Start(":8000"))
+	e.POST("/cars", createCar)
+	e.Logger.Fatal(e.Start(":8080"))
 }
 
 func getCars (c echo.Context) error {
+	return c.JSON(200, cars)
+}
+
+func createCar (c echo.Context) error{
+	car := new(Car)
+	if err != c.Bind(car); err != nil {
+		retur err
+	}
+	cars = append(cars, *car)
 	return c.JSON(200, cars)
 }
